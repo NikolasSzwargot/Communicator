cb68e7c6de34baa09c40d3430b9b916dd0d906ad
Nikolas Szwargot <nszwargot@gmail.com>
2023-12-08 14:52:24 +0100

    Added an option for a server to connect with many clients.

--- server.cpp
+++ server.cpp
@@ -3,29 +3,46 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <pthread.h>
+
+void *handleClient(void *arg) {
+    int clientSocket = *((int *)arg);
+    char buffer[1024];
+    ssize_t bytesRead;
+
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+        buffer[bytesRead] = '\0';
+        std::cout << "Otrzymana wiadomość od klienta: " << buffer << std::endl;
+    }
+
+    if (bytesRead == 0) {
+        std::cout << "Klient się rozłączył." << std::endl;
+    } else if (bytesRead == -1) {
+        std::cerr << "Błąd podczas odbierania danych." << std::endl;
+    }
+
+    close(clientSocket);
+    pthread_exit(NULL);
+}
 
 int main() {
-    // Tworzenie gniazda serwera
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == -1) {
         std::cerr << "Błąd podczas tworzenia gniazda." << std::endl;
         return -1;
     }
 
-    // Struktura opisująca adres serwera
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(12345); // Port serwera
-    serverAddress.sin_addr.s_addr = INADDR_ANY; // Akceptuj połączenia od dowolnego adresu
+    serverAddress.sin_port = htons(12345);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    // Przypisanie adresu do gniazda serwera
     if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
         std::cerr << "Błąd podczas przypisywania adresu do gniazda." << std::endl;
         close(serverSocket);
         return -1;
     }
 
-    // Nasłuchiwanie na gnieździe serwera
     if (listen(serverSocket, 5) == -1) {
         std::cerr << "Błąd podczas nasłuchiwania na gnieździe." << std::endl;
         close(serverSocket);
@@ -34,27 +51,21 @@ int main() {
 
     std::cout << "Serwer nasłuchuje na porcie 12345..." << std::endl;
 
-    // Akceptowanie połączeń od klientów
-    int clientSocket = accept(serverSocket, nullptr, nullptr);
-    if (clientSocket == -1) {
-        std::cerr << "Błąd podczas akceptowania połączenia." << std::endl;
-        close(serverSocket);
-        return -1;
+    while (true) {
+        int clientSocket = accept(serverSocket, nullptr, nullptr);
+        if (clientSocket == -1) {
+            std::cerr << "Błąd podczas akceptowania połączenia." << std::endl;
+            close(serverSocket);
+            return -1;
+        }
+
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, handleClient, (void *)&clientSocket) != 0) {
+            std::cerr << "Błąd podczas tworzenia wątku." << std::endl;
+            close(clientSocket);
+        }
     }
 
-    // Odbieranie wiadomości od klienta
-    char buffer[1024];
-    ssize_t bytesRead;
-    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)))
-    {
-        //bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-        buffer[bytesRead] = '\0';
-        std::cout << "Otrzymana wiadomość od klienta: " << buffer << std::endl;
-    }
-
-    // Zamykanie gniazda klienta i serwera
-    close(clientSocket);
     close(serverSocket);
-
     return 0;
 }
